a323c03e118d4f7a864d9d58547282e3cba02231
chenxiaojie <chenxiaojie2@yy.com>
2017-12-20 17:41:26 +0800

    处理巨硬\r\n换行的问题

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ var fs = require('fs');
 var path = require('path');
 var glob = require('glob');
 var crypto = require('crypto');
+var os = require('os');
 
 var hashFileObjects = [];
 var filePatterns = [
@@ -38,6 +39,9 @@ function StaticResHashPlugin(options) {
   if (options.destRoot === undefined) {
     console.error('destRoot is needed!');
   }
+  if (options.tplRoot === undefined) {
+    // console.error('tplRoot is needed!');
+  }
   if (options.publicPath === undefined) {
     console.error('publicPath is needed!');
   }
@@ -58,10 +62,19 @@ StaticResHashPlugin.prototype.apply = function (compiler) {
     var that = this;
     setTimeout(function () {
       globOpts = {
-        root: path.resolve(path.join(process.cwd(), that.options.destRoot)),
+        root: path.resolve(that.options.destRoot),
         nodir: true,
         log: !!that.options.log
       };
+
+      if (that.options.tplRoot != undefined) {
+        tplGlobOpts = {
+          root: path.resolve(that.options.tplRoot),
+          nodir: true,
+          log: !!that.options.log
+        };
+      }
+
 
       globOpts.log && console.log('current context path : ' + process.cwd());
 
@@ -78,7 +91,14 @@ StaticResHashPlugin.prototype.apply = function (compiler) {
           };
           obj.ext = path.extname(tmpFiles[j]);
           obj.name = path.basename(tmpFiles[j], obj.ext);
-          obj.hashBaseName = obj.name + '.' + obj.hash + obj.ext;
+          var oldHash = obj.name.substr(obj.name.lastIndexOf('.') + 1)
+          // console.log("oldHash: "+ oldHash)
+          if (oldHash == obj.hash) {
+            obj.hashBaseName = obj.name + obj.ext;
+          } else {
+            obj.hashBaseName = obj.name + '.' + obj.hash + obj.ext;
+          }
+
           obj.hashPath = path.join(path.dirname(obj.path), obj.hashBaseName);
 
           hashFilExist = false;
@@ -93,15 +113,25 @@ StaticResHashPlugin.prototype.apply = function (compiler) {
       }
 
       var logData = {}
-      if (fs.existsSync(that.options.outputFile)) {
-        var outputFile = fs.readFileSync(that.options.outputFile, 'utf8');
-        logData = JSON.parse(outputFile)
-      }
+      // if (fs.existsSync(that.options.outputFile)) {
+      //   var outputFile = fs.readFileSync(that.options.outputFile, 'utf8');
+      //   logData = JSON.parse(outputFile)
+      // }
 
       for (i = 0; i < hostFiles.length; i++) {
         tmpFiles = glob.sync(hostFiles[i], globOpts);
+        tplFiles = [];
+        //tplRoot参数不传时不错模板扫描
+        if (that.options.tplRoot != undefined && that.options.tplRoot != "") {
+          tplFiles = glob.sync(hostFiles[i], tplGlobOpts);
+        }
+
         for (j = 0; j < tmpFiles.length; j++) {
-          updateHashRef(tmpFiles[j], that.options.publicPath, logData);
+          updateHashRef(tmpFiles[j], that.options.publicPath, logData, true);
+        }
+
+        for (j = 0; j < tplFiles.length; j++) {
+          updateHashRef(tplFiles[j], that.options.publicPath, logData, false);
         }
       }
 
@@ -121,13 +151,34 @@ StaticResHashPlugin.prototype.apply = function (compiler) {
 
 
 function calcHashByFile(fp, length) {
-  var cont = fs.readFileSync(fp, 'utf8');
+  var cont = fs.readFileSync(fp);
+  if (os.platform() == 'win32' && fp.match("\.js|\.css") != null) {
+    cont = Dos2Unix(cont)
+  }
+
   var shaHasher = crypto.createHash('sha1');
   shaHasher.update(cont);
   return shaHasher.digest('hex').slice(0, length);
 }
 
-function updateHashRef(hostFilePath, publicPath, logObj) {
+function Dos2Unix(buf) {
+  let arr = []
+  for (let i = 0; i < buf.length; i++) {
+    let m = buf[i]
+    if (i == buf.length - 1) {
+      arr.push(m)
+      break
+    }
+    let af = buf[i + 1]
+    if (m == 0x0d && af == 0x0a) {
+      continue
+    }
+    arr.push(m)
+  }
+  return new Buffer(arr)
+}
+
+function updateHashRef(hostFilePath, publicPath, logObj, replace) {
   if (publicPath[publicPath.length - 1] == "/") {
     publicPath = publicPath.substr(0, publicPath.length - 1)
   }
@@ -168,7 +219,10 @@ function updateHashRef(hostFilePath, publicPath, logObj) {
           if (hashFileObjects[j].path === linkAbsPath) {
             globOpts.log && console.log('file [' + hostFilePath + '] execute replace [' + link + '] --> [' + hashFileObjects[j].hashBaseName + ']')
             logObj[pre + link] = pre + path.join(path.dirname(link), hashFileObjects[j].hashBaseName).replace(/\\/g, '/')
-            return sm01 + pre + path.join(path.dirname(link), hashFileObjects[j].hashBaseName).replace(/\\/g, '/') + queryString + sm03;
+            if (replace) {
+              return sm01 + pre + path.join(path.dirname(link), hashFileObjects[j].hashBaseName).replace(/\\/g, '/') + queryString + sm03;
+            }
+            return cont;
           }
         }
       }
